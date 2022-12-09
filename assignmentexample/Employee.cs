1b7f584e412f9fc11c3698de1beaa19f8be18159
VPraveenAchari <119933554+VPraveenAchari@users.noreply.github.com>
2022-12-09 15:38:14 +0530

    Add files via upload

---
+++ assignmentexample/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment.Model;
+
+namespace Assignment
+{
+    public class Employee
+    {
+        List<EmployeeModel> emp=new List<EmployeeModel>();
+        public void AddEmployee(EmployeeModel employee)
+        {
+            emp.Add(employee);
+        }
+        public void DeleteEmployee(EmployeeModel employee)
+        {
+            emp.Remove(employee);
+
+        }
+    }
+}
---
+++ assignmentexample/Program.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+
+namespace Assignment
+{
+    class Practice
+    {
+        List<Employee> list = new List<Employee>();
+        void AddEmployees()
+        {
+            Console.Write("Enter Employee Id:");
+            int Id = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Employee Name:");
+            string Name =Console.ReadLine();
+            Console.Write("Enter Employee Salary:");
+            int Salary =Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Employee Gender:");
+            string Gender =Console.ReadLine();
+            Employee emp = new Employee();
+            emp.AddEmployee(new Model.EmployeeModel
+            {
+                Id = Id,
+                Name = Name,
+                Salary = Salary,
+                Gender = Gender
+            });
+            Console.WriteLine("Employee Added Successfully");
+        }
+        
+        void DeleteEmployee()
+        {
+
+        }
+        
+
+        void Sample()
+        {
+            while (true)
+            {
+                Console.WriteLine("Select any one option");
+                Console.WriteLine("1.Add an Employee");
+                Console.WriteLine("2.Delete an Employee");
+                Console.WriteLine("3.Display all Employees");
+                Console.WriteLine("4.Exit");
+                string input = Console.ReadLine();
+                if (input == "4") break;
+                switch (input)
+                {
+                     case "1":
+                           Console.WriteLine("Add Employee");
+                           AddEmployees();
+                           break;
+                    case "2":
+                           Console.WriteLine("Delete Employee");
+                           //DeleteEmployees();
+                           break;
+                    case "3":
+                           Console.WriteLine("Print All Employees");
+                          // PrintEmployees();
+                          // PrintEmployees();
+                           break;
+                }
+            }
+        }
+        static void Main(String[] args)
+        {
+           Practice p = new Practice();  
+            p.Sample();
+        }
+    }
+}
