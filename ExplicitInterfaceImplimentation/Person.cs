33b1f75898463776b8b250014717468213f8b363
VPraveenAchari <119933554+VPraveenAchari@users.noreply.github.com>
2023-01-19 21:23:41 +0530

    Add files via upload

---
+++ ExplicitInterfaceImplimentation/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitInterface
+{
+    public class Person:I1,I2
+    {
+      void I1.Calculate()
+        {
+            Console.WriteLine("Enter Cgpa");
+            double cgpa = Convert.ToDouble(Console.ReadLine());
+
+            double percentage = cgpa * 9.5;
+            Console.WriteLine("Percentage is:{0}", percentage);
+        }
+        void I2.Calculate()
+        {
+            Console.WriteLine("Enter Salary");
+            int salary = Convert.ToInt32(Console.ReadLine());
+            int TotalSalary = salary + salary * 5 / 100;
+            Console.WriteLine("TotalSalary is:{0}", TotalSalary);
+        }
+    }
+}
---
+++ ExplicitInterfaceImplimentation/Program.cs
@@ -0,0 +1,10 @@
+﻿using ExplicitInterface;
+
+/*I1 i1 = new Person();
+I2 i2 = new Person();
+
+i1.Calculate();
+i2.Calculate();*/
+Person p = new Person();
+((I1)p).Calculate();
+((I2)p).Calculate();
