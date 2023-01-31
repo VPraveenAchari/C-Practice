498f28318c5486829a6216bbb365382c412f4a27
VPraveenAchari <119933554+VPraveenAchari@users.noreply.github.com>
2023-01-31 21:46:34 +0530

    Add files via upload

---
+++ Indexers/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexersExample
+{
+    public class Employee
+    {
+        int empid,salary;
+        string ename, job, location;
+        public Employee(int empid, string ename, string job, string location, int salary)
+        {
+            this.empid = empid;
+            this.ename= ename;
+            this.job = job;
+            this.location = location;
+            this.salary = salary;
+        }
+        public object this[int index]
+        {
+            get { if (index == 1)
+                    return empid;
+                else if (index == 2)
+                    return ename;
+                else if (index == 3)
+                    return job;
+                else if (index == 4)
+                    return location;
+                else if (index == 5) 
+                    return salary;
+                   return null;
+            }
+            set
+            {
+                if (index == 1)
+                    empid = (int)value;
+                else if(index==2)
+                    ename =(string) value;
+                else if(index==3)
+                    job=(string) value;
+                else if(index==4)
+                    location=(string) value;
+                else if(index==5)
+                    salary=(int) value;
+            }
+        }
+    }
+}
---
+++ Indexers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace IndexersExample
+{
+    class Indexers
+    {
+        public static void Main(string[] args)
+        {
+            Employee idx = new Employee(1001, "SaiKiran", "Manager", "Hyderabad",45000);
+            idx[3] = "Clerk";
+            Console.WriteLine("Employee NO:" + idx[1]);
+            Console.WriteLine("Employee Name:" + idx[2]);
+            Console.WriteLine("Employee Job:" + idx[3]);
+            Console.WriteLine("Employee Location:" + idx[4]);
+            Console.WriteLine("Employee Salary:" + idx[5]);
+        }
+    }
+}
