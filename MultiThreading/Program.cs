55119e37230c6e079ba4155d10004e24b9a0bd4f
VPraveenAchari <119933554+VPraveenAchari@users.noreply.github.com>
2023-02-06 23:18:46 +0530

    Add files via upload

---
+++ MultiThreading/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+namespace ThreadLockingExample
+{
+    class ThreadLocking
+    {
+        public void Display()
+        {
+            lock (this)
+            {
+                Console.Write("[C# is");
+                Thread.Sleep(5000);
+                Console.WriteLine(" Object Oriented Programming]");
+            }
+        }
+        public static void Main(string[]args)
+        {
+            ThreadLocking t = new ThreadLocking();
+            Thread t1 = new Thread(t.Display);
+            Thread t2 = new Thread(t.Display);
+            Thread t3 = new Thread(t.Display);
+            t1.Start();
+            t2.Start();
+            t3.Start();
+        }
+    }
+}
