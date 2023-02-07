fec329ac62b9085ccd4798971b01d7d9f6f8d789
VPraveenAchari <119933554+VPraveenAchari@users.noreply.github.com>
2023-02-07 21:50:00 +0530

    Add files via upload

---
+++ Properties/Customer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerProperties
+{
+    public class Customer
+    {
+        int _Custid;
+        string _Cname;
+        bool _Status;
+        double _Balance;
+        Cities _City;
+        public Customer(int custid, string cname, bool status, double balance,Cities City)
+        {
+            _Custid = custid;
+            _Cname = cname;
+            _Status = status;
+            _Balance = balance;
+            _City= City;
+        }
+        public int Custid
+        {
+            get { return _Custid; }
+        }
+        public bool Status
+        {
+            get { return _Status; }
+            set { _Status = value; }
+        }
+        public string Cname
+        {
+            get { return _Cname; }
+            set { _Cname = value; }
+        }
+        public double Balance
+        {
+            get { return _Balance; }
+            set {
+                if (value>=500)
+                {
+                    _Balance = value;
+                }
+                 }
+        }
+       public Cities City
+        {
+            get { return _City; }
+            set { 
+                if(_Status==true)
+                {
+                    _City = value;
+                }
+            }
+        }
+    }
+}
---
+++ Properties/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CustomerProperties;
+
+namespace CustomerProperties
+{
+    class TestCustomer
+    {
+       public static void Main(string[] args)
+        {
+            Customer c = new Customer(101,"Praveen",false,5000.00,Cities.Delhi);
+            Console.WriteLine("Customer id:"+c.Custid);
+            c.Status = true;
+            if(c.Status==true)
+            {
+                Console.WriteLine("Status is in Active");
+            }
+            else
+            {
+                Console.WriteLine("Status is in InActive");
+            }
+            c.Balance = c.Balance - 4600;
+            Console.WriteLine("Customer Name is:"+c.Cname);
+            Console.WriteLine("Available Balance"+c.Balance);
+            Console.WriteLine("Current City"+c.City);
+            c.City=Cities.Hyderabad;
+            Console.WriteLine("Modified City" + c.City);
+        }
+    }
+}
