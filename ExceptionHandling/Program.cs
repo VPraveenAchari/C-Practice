36cdd271ac05971961fd22e49a61194578c79dbf
VPraveenAchari <119933554+VPraveenAchari@users.noreply.github.com>
2023-02-20 21:57:31 +0530

    Add files via upload

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ExceptionHandlingExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a, b;
+                Console.WriteLine("Enter the First number");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the Second Number");
+                b = Convert.ToInt32(Console.ReadLine());
+                int c = a / b;
+                Console.WriteLine("Result is" + a / b);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
